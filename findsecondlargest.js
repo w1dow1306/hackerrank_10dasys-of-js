22e999da27bce8603db069f34506a8803eed402b
Avinash khalkho <102430545+w1dow1306@users.noreply.github.com>
2023-06-04 13:13:01 -0700

    Update findsecondlargest.js

--- findsecondlargest.js
+++ findsecondlargest.js
@@ -1,1 +1,13 @@
 
+function getSecondLargest(nums) {
+    nums = nums.sort().reverse();
+    let n = nums.length;
+    for(let i = 0;i<n;i++){
+        if((nums[0]>nums[i])){
+            return(nums[i]);
+            break;
+        }else{
+            nums[0]=nums[i];
+        }
+    }
+}
